50d61431b3b519319623250a23a8dc7b577b864e
BorysenkoLiza <borysenko.yelyzaveta@gmail.com>
2024-06-04 15:40:16 +0300

    Updated app.py to use dataframes. Show doc name instead of id

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from werkzeug.utils import secure_filename
 from modules.data_loader import DataLoader
 from modules.semantic_clusterer import SemanticClusterer
 from modules.lexical_proximity_algorithm import LexicalProximityAlgorithm
+from modules.jaccard import JaccardSimilarityCalculator
 import secrets  # To generate a secret key
 
 # Set up logging
@@ -48,74 +49,58 @@ def upload():
 def process_zip(zip_path):
     # Create a temporary directory for extraction
     temp_extract_dir = tempfile.mkdtemp()
-
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(temp_extract_dir)
-    
-    # Create the final extraction directory
+    # Move all .txt files to the final extraction directory
     extract_folder = os.path.join(app.config['UPLOAD_FOLDER'], os.path.splitext(os.path.basename(zip_path))[0])
     os.makedirs(extract_folder, exist_ok=True)
-
-    # Move all .txt files to the final extraction directory
     for root, dirs, files in os.walk(temp_extract_dir):
         for file in files:
             if file.endswith('.txt'):
-                full_path = os.path.join(root, file)
-                new_path = os.path.join(extract_folder, file)
-                shutil.move(full_path, new_path)
-
-    # Cleanup the temporary directory
+                shutil.move(os.path.join(root, file), os.path.join(extract_folder, file))
     shutil.rmtree(temp_extract_dir)
-    
     return extract_folder
 
 def process_documents(extract_folder, num_clusters, shingle_size):
     # Initialize DataLoader and load documents as sets of shingles
     logger.info("Initializing DataLoader and loading documents...")
     loader = DataLoader(extract_folder, shingle_size)
-    docs_as_sets, documents = loader.get_docs_as_sets_and_texts()
+    documents_df = loader.get_docs_as_sets_and_texts()
 
     # Initialize the SemanticClusterer
     logger.info("Initializing SemanticClusterer and processing documents...")
-    clusterer = SemanticClusterer(num_clusters)  # Assuming SemanticClusterer accepts num_clusters
-    labels, silhouette = clusterer.process_documents([doc[1] for doc in documents])
-    logger.info("Cluster Labels: %s", labels)
+    clusterer = SemanticClusterer(num_clusters)
+    documents_df, silhouette = clusterer.process_documents(documents_df, train_new_model=False)
     logger.info("Silhouette Score: %f", silhouette)
 
     # Grouping documents by clusters
-    clusters = {}
-    for (doc_id, doc_text), label in zip(documents, labels):
-        if label not in clusters:
-            clusters[label] = []
-        clusters[label].append(doc_id)  # Store only doc_id for later reference
-
-    # Debugging: Check if clusters are populated
-    logger.info(f"Number of clusters: {len(clusters)}")
-    for label, doc_ids in clusters.items():
-        logger.info(f"Cluster {label} has {len(doc_ids)} documents")
+    cluster_groups = documents_df.groupby('Cluster')
 
     # Similarity for docs in each cluster
     cluster_similarities = {}
-    if not clusters:
-        logger.warning("No clusters found. Please check the clustering process.")
-    else:
-        for label, doc_ids in clusters.items():
-            logger.info(f"Running MinHash for Cluster {label}")
-            try:
-                cluster_docs_as_sets = {doc_id: docs_as_sets[doc_id] for doc_id in doc_ids}
-            except KeyError as e:
-                logger.error(f"KeyError: {e} - Check if doc_id is present in docs_as_sets")
-                continue
-            minhash = LexicalProximityAlgorithm(cluster_docs_as_sets, num_hashes=100, similarity_threshold=0.5)
+    clusters = {}
+    logger.info(f"Number of clusters: {len(cluster_groups)}")
+
+    for label, group in cluster_groups:
+        logger.info(f"Running MinHash for Cluster {label}")
+        cluster_docs_df = group[['DocID','DocName','Shingles']].copy() 
+        #Convert DataFrame to dictionary format: {DocID: shingles_set, ...}
+        docs_as_sets = {row['DocID']: row['Shingles'] for index, row in cluster_docs_df.iterrows()}
+        clusters[label] = list(cluster_docs_df['DocName'])  # Store document names for the cluster
+        try:
+            minhash = LexicalProximityAlgorithm(docs_as_sets, num_hashes=100, similarity_threshold=0.5)
             signatures = minhash.generate_minhash_signatures()
             similarities = minhash.calculate_similarities(signatures)
-
             similar_pairs = sorted(similarities, key=lambda x: x[2], reverse=True)[:5] #top five similar pairs for cluster
             cluster_similarities[label] = similar_pairs
-
+            #similarity_calculator = JaccardSimilarityCalculator(docs_as_sets)
+            #jaccard_similarities = similarity_calculator.calculate_jaccard()
             for doc1, doc2, similarity in similarities:
                 if similarity > 0.1:  # Adjust threshold as needed
                     logger.info(f"Cluster {label}: Document {doc1} is similar to Document {doc2} with similarity {similarity:.8f}")
+        except KeyError as e:
+            logger.error(f"KeyError: {e} - Check if DocID is present in docs_as_sets")
+
     cluster_top_terms = clusterer.get_top_terms_per_cluster()
     session['clusters'] = {int(k): v for k, v in clusters.items()}  # Convert keys to int
     session['cluster_top_terms'] = {int(k): v for k, v in cluster_top_terms.items()}  # Convert keys to int
