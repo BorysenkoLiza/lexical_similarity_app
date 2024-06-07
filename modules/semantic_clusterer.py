70d39bfce5dc0c5c25e3a847896bc2e17a5e22e4
BorysenkoLiza <borysenko.yelyzaveta@gmail.com>
2024-06-07 21:41:23 +0300

    Refactored semantic clusterer

--- modules/semantic_clusterer.py
+++ modules/semantic_clusterer.py
@@ -4,20 +4,19 @@ import logging
 import time
 from typing import Counter
 
+import numpy as np
 import matplotlib.pyplot as plt
+
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-
-import nltk
-import numpy as np
-
-from gensim.models import Word2Vec, KeyedVectors
-
-from nltk import word_tokenize
-from nltk.corpus import stopwords as nltk_stopwords
-
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.metrics import silhouette_score, silhouette_samples
+
+from gensim.models import KeyedVectors
+
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords as nltk_stopwords
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,12 +27,12 @@ class SemanticClusterer:
     """
     A class to perform document clustering using Word2Vec vectors and MiniBatchKMeans.
     
-    This class loads a pretrained Word2Vec model, preprocesses text documents, vectorizes them, and then clusters the vectors
-    using the MiniBatchKMeans algorithm. It also evaluates the clustering quality using silhouette scores.
+    This class loads a pretrained Word2Vec model, tokenizes text documents, vectorizes them, and then clusters the vectors
+    using the MiniBatchKMeans.
     
     Attributes:
         vector_size (int): Dimension of the Word2Vec vectors.
-        min_cluster (int): Minimum number of clusters to use in KMeans.
+        num_cluster (int): Number of clusters to use
         batch_size (int): Batch size for MiniBatchKMeans.
         seed (int): Seed for random number generation.
         stopwords (set): A set of stopwords to use.
@@ -42,33 +41,34 @@ class SemanticClusterer:
     
     Methods:
         clean_text(text): Pre-process text and generate tokens.
-        vectorize_docs(documents): Convert tokenized documents into vectors using the Word2Vec model.
+        tokenize_documents(documents): Tokenizes a list of documents.
+        vectorize_documents(tokenized_documents, strategy): Convert tokenized documents into vectors using the Word2Vec model.
         cluster_documents(vectors): Cluster document vectors using MiniBatchKMeans.
         get_top_terms_per_cluster(): Return the top terms for each cluster based on the cluster centroids.
-        process_documents(raw_documents): Process documents from text to clusters.
+        print_silhouette_scores(labels, sample_silhouette_values): Print the silhouette scores for each cluster.
+        get_top_terms_per_cluster(documents_df): Return the top terms for each cluster based on the cluster centroids.
+        process_documents(documents_df): Process documents from text to clusters.
+        visualize_clusters(vectors, labels, method, filename): Visualize the clustered document vectors using PCA or t-SNE and save the plot as an image.
     """
-    def __init__(self, vector_size=300, min_cluster=5, batch_size=500, seed=42):
+    def __init__(self, vector_size=300, num_cluster=5, batch_size=500, seed=42):
         """
         Initialize the SemanticClusterer with the specified parameters and load the pretrained Word2Vec model.
         
         Parameters:
             vector_size (int, optional): Dimension of the Word2Vec vectors. Defaults to 300.
-            min_cluster (int, optional): Minimum number of clusters to use in KMeans. Defaults to 10.
+            num_cluster (int, optional): Number of clusters to use in KMeans. Defaults to 10.
             batch_size (int, optional): Batch size for MiniBatchKMeans. Defaults to 500.
             seed (int, optional): Seed for random number generation. Defaults to 42.
         """
         self.vector_size = vector_size
-        self.min_cluster = min_cluster
+        self.num_cluster = num_cluster
         self.batch_size = batch_size
         self.seed = seed
         self.stopwords = set(nltk_stopwords.words('english'))
         np.random.seed(seed)
         self.km = None
         start_time = time.time()
-        self.model = KeyedVectors.load_word2vec_format(
-            'D:/uni/4 курс/2 семестр/Диплом/git_thesis/Lexical_proximity/modules/GoogleNews-vectors-negative300.bin',
-            binary=True
-        )
+        self.model = KeyedVectors.load_word2vec_format('C:/Users/borys/thesis_utils/GoogleNews-vectors-negative300.bin', binary=True)
         elapsed_time = time.time() - start_time
         logger.info("Word2Vec model loaded in %.2f seconds", elapsed_time)
 
@@ -93,33 +93,35 @@ class SemanticClusterer:
         return tokens
     
     def tokenize_documents(self, documents):
+        """
+        Tokenizes a list of documents.
+        
+        Parameters:
+            documents (List[str]): List of raw text documents.
+        
+        Returns:
+            List[List[str]]: List of tokenized documents.
+        """
         tokenized_docs = [self.clean_text(doc) for doc in documents]
         return tokenized_docs
 
-    def vectorize_docs(self, tokenized_documents, strategy="average"):
+    def vectorize_documents(self, tokenized_documents, strategy="average"):
         """
         Convert tokenized documents into vectors using the Word2Vec model.
         
         Parameters:
-            tokenized_documents (list of list of str): Tokenized documents.
+            tokenized_documents (List[List[str]]): Tokenized documents.
             strategy (str): Aggregation strategy ("average", or "min-max").
         
         Returns:
-            list: List of document vectors.
+            List[np.ndarray]: List of document vectors.
         """
         features = []
         size_output = self.model.vector_size
         embedding_dict = self.model
 
         for tokens in tokenized_documents:
-            zero_vector = np.zeros(size_output)
-            vectors = []
-            for token in tokens:
-                if token in embedding_dict:
-                    try:
-                        vectors.append(embedding_dict[token])
-                    except KeyError:
-                        continue
+            vectors = [embedding_dict[token] for token in tokens if token in embedding_dict]
             if vectors:
                 vectors = np.asarray(vectors)
                 if strategy == "min-max":
@@ -132,23 +134,8 @@ class SemanticClusterer:
                 else:
                     raise ValueError(f"Aggregation strategy {strategy} does not exist!")
             else:
-                features.append(zero_vector)
+                features.append(np.zeros(size_output))
         return features
-    
-    #def train_word2vec_model(self, tokenized_documents):
-        """
-        Train a Word2Vec model on the provided tokenized documents.
-        
-        Parameters:
-            tokenized_documents (list of list of str): Tokenized documents.
-        
-        Returns:
-            Word2Vec: Trained Word2Vec model.
-        """
-        model = Word2Vec(sentences=tokenized_documents, vector_size=self.vector_size, workers=1, seed=self.seed)
-        self.model = model.wv
-        logger.info("Word2Vec model trained successfully.")
-        return model
 
     def cluster_documents(self, vectors):
         """
@@ -158,10 +145,10 @@ class SemanticClusterer:
             vectors (np.array): Document vectors.
         
         Returns:
-            tuple: Cluster labels and silhouette score.
+            Tuple[np.ndarray, float]: Cluster labels and silhouette score.
         """
         start_time = time.time()
-        self.km = MiniBatchKMeans(n_clusters=self.min_cluster, batch_size=self.batch_size, random_state=self.seed)
+        self.km = MiniBatchKMeans(n_clusters=self.num_cluster, batch_size=self.batch_size, random_state=self.seed)
         labels = self.km.fit_predict(vectors)
         elapsed_time = time.time() - start_time
         logger.info("Documents clusterized successfully in %.2f seconds", elapsed_time)
@@ -171,6 +158,7 @@ class SemanticClusterer:
 
         # Print silhouette values for each cluster
         self.print_silhouette_scores(labels, sample_silhouette_values)
+        # Generate visualization 
         self.visualize_clusters(vectors, labels, method='pca', filename='static/cluster_visualization.png')
         return labels, silhouette_avg
     
@@ -182,7 +170,7 @@ class SemanticClusterer:
             labels (np.array): Cluster labels for each document.
             sample_silhouette_values (np.array): Silhouette values for each document.
         """
-        for i in range(self.min_cluster):
+        for i in range(self.num_cluster):
             cluster_silhouette_values = sample_silhouette_values[labels == i]
             logger.info(f"Cluster {i}:")
             logger.info(f"    Number of samples: {cluster_silhouette_values.shape[0]}")
@@ -192,44 +180,40 @@ class SemanticClusterer:
     
     def get_top_terms_per_cluster(self, documents_df):
         """
-        Get the top terms for each cluster based on frequency.
+        Get the top ten terms for each cluster based on frequency.
         
         Parameters:
             documents_df (pandas.DataFrame): DataFrame with 'Cluster' and 'Tokens' columns.
         
         Returns:
-            dict: A dictionary with cluster numbers as keys and strings of top terms and their frequencies as values.
+            Dict[int, List[Tuple[str, int]]]: A dictionary with cluster numbers as keys and strings of top terms and their frequencies as values.
         """
         logger.info("Top terms per cluster (based on frequency):")
         top_terms = {}
-        for i in range(self.min_cluster):
+        for i in range(self.num_cluster):
             cluster_docs = documents_df[documents_df['Cluster'] == i]['Tokens']
             all_tokens = [token for sublist in cluster_docs for token in sublist]
             most_frequent = Counter(all_tokens).most_common(10)
             top_terms[i] = most_frequent
         return top_terms
 
-    def process_documents(self, documents_df, train_new_model=False):
+    def process_documents(self, documents_df):
         """
         Processes documents from DataFrame to clusters.
         
         Parameters:
-            documents_df (pandas.DataFrame): DataFrame with columns 'DocID' and 'DocText'.
+            documents_df (d.DataFrame): DataFrame with columns 'DocID' and 'DocText'.
         
         Returns:
-            pandas.DataFrame: DataFrame with additional columns for tokenized texts, vectors, and cluster labels.
+             Tuple[pd.DataFrame, float, Dict[int, List[Tuple[str, int]]]]: DataFrame (with additional columns for tokenized texts, cluster labels), silhouette and top ten terms for each cluster 
         """
         start_time1 = time.time()
         documents_df['Tokens'] = documents_df['DocText'].apply(self.clean_text)
         elapsed_time1 = time.time() - start_time1
         logger.info("Documents preproccessed and tokenized in %.2f seconds", elapsed_time1)
 
-        #if train_new_model:
-            #self.train_word2vec_model(documents_df['Tokens'].tolist())
-
-        # Get document vectors
         start_time2 = time.time()
-        document_vectors = self.vectorize_docs(documents_df['Tokens'].tolist())
+        document_vectors = self.vectorize_documents(documents_df['Tokens'].tolist())
         elapsed_time2 = time.time() - start_time2
         logger.info("Document vectors created in %.2f seconds", elapsed_time2)
 
